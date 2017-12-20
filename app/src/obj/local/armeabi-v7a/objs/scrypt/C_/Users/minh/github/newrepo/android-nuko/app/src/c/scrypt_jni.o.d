1e80eb33d92b43f7fa4ccaee564ef938994655c4
randomfolk <vortex2910@gmail.com>
2017-12-20 12:20:47 +0800

    add nihongo

---
+++ app/src/obj/local/armeabi-v7a/objs/scrypt/C_/Users/minh/github/newrepo/android-nuko/app/src/c/scrypt_jni.o.d
@@ -0,0 +1,5 @@
+C:\Users\minh\github\newrepo\android-nuko\app\src/obj/local/armeabi-v7a/objs/scrypt/C_\Users\minh\github\newrepo\android-nuko\app\src/c/scrypt_jni.o: \
+  C:\Users\minh\github\newrepo\android-nuko\app\src/c/scrypt_jni.c \
+  C:\Users\minh\github\newrepo\android-nuko\app\src/include\crypto_scrypt.h
+
+C:\Users\minh\github\newrepo\android-nuko\app\src/include\crypto_scrypt.h:
---
+++ app/src/obj/local/x86_64/objs/scrypt/C_/Users/minh/github/newrepo/android-nuko/app/src/c/scrypt_jni.o.d
@@ -0,0 +1,5 @@
+C:\Users\minh\github\newrepo\android-nuko\app\src/obj/local/x86_64/objs/scrypt/C_\Users\minh\github\newrepo\android-nuko\app\src/c/scrypt_jni.o: \
+  C:\Users\minh\github\newrepo\android-nuko\app\src/c/scrypt_jni.c \
+  C:\Users\minh\github\newrepo\android-nuko\app\src/include\crypto_scrypt.h
+
+C:\Users\minh\github\newrepo\android-nuko\app\src/include\crypto_scrypt.h:
